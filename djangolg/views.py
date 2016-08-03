9af6dd5a092e095e6e07f9fea39080ff98a8a18d
Ben Maddison <benm@workonline.co.za>
2016-08-03 15:08:34 +0200

    added session start logging

--- djangolg/views.py
+++ djangolg/views.py
@@ -43,18 +43,24 @@ class AcceptTermsView(View):
     def get(self, request):
         response = {'status': 'error'}
         query = request.GET
+        src_host = get_src(self.request)
         if query:
             if settings.RECAPTCHA_ON:
                 recaptcha = {
                     'recaptcha_resp': query['g-recaptcha-response'],
                     'secret_key': settings.RECAPTCHA_SECRET_KEY,
-                    'src_address': get_src(self.request)
+                    'src_address': src_host
                 }
                 form = forms.RecaptchaTermsForm(recaptcha)
             else:
                 form = forms.AcceptTermsForm(query)
             if form.is_valid():
                 key = keys.AuthKey(get_src(self.request))
+                log = models.Log(
+                    event=models.Log.EVENT_START,
+                    src_host=src_host,
+                    key=key
+                ).save()
                 response = {
                     'status': 'ok',
                     'key': key.signed
