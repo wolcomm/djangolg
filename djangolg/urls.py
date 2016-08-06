2bfd78763990f013a029c5e92e04730384d9b7ad
Ben Maddison <benm@workonline.co.za>
2016-08-06 17:12:30 +0200

    json based request handling and error display

--- djangolg/urls.py
+++ djangolg/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name="djangolg-index"),
     url(r'^enter/$', views.AcceptTermsView.as_view(), name="djangolg-enter"),
     url(r'^lg/$', views.LookingGlassHTMLView.as_view(), name="djangolg-lg"),
+    url(r'^json/$', views.LookingGlassJsonView.as_view(), name="djangolg-json"),
 ]
--- djangolg/views.py
+++ djangolg/views.py
@@ -83,7 +83,7 @@ class LookingGlassHTMLView(TemplateView):
         if query:
             key = query['auth_key']
             log.key = key
-            if self.authorise(key=key, src_host=src_host):
+            if authorise(key=key, src_host=src_host):
                 log.event = models.Log.EVENT_QUERY_ACCEPT
                 method = methods.Method(query['method_name'])
                 if method:
@@ -92,7 +92,7 @@ class LookingGlassHTMLView(TemplateView):
                     if form.is_valid():
                         log.router = form.cleaned_data['router']
                         log.target = form.cleaned_data['target']
-                        context['output'] = self.execute(form=form, method=method)
+                        context['output'] = execute(form=form, method=method)
                     else:
                         log.event = models.Log.EVENT_QUERY_INVALID
                         log.error = "Form validation failed"
@@ -107,44 +107,41 @@ class LookingGlassHTMLView(TemplateView):
         log.save()
         return context
 
-    def authorise(self, key=None, src_host=None):
-        if keys.AuthKey(src_host).validate(key):
-            count = models.Log.objects.filter(key=key).count()
-            if not settings.MAX_REQUESTS or count < settings.MAX_REQUESTS:
-                return True
-        return False
-
-    def execute(self, form, method):
-        data = form.cleaned_data
-        router = data['router']
-        target = data['target']
-        if 'options' in data:
-            option = int(data['options'])
-        else:
-            option = None
-        lg = LookingGlass(router=router)
-        output = lg.execute(
-            method=method,
-            target=target,
-            option=option,
-        )
-        return output
-
 
 class LookingGlassJsonView(View):
     def get(self, request):
         query = request.GET
+        response = {
+            'err': None,
+            'raw': None,
+        }
         if not query:
             return JsonResponse({}, status=400)
-        method = methods.Method(query['method_name'])
-        if method:
-            form = method.form(query)
-            if form.is_valid():
-                response = { 'output': form.execute() }
+        src_host = get_src(self.request)
+        log = models.Log(src_host=src_host)
+        key = query['auth_key']
+        log.key = key
+        if authorise(key=key, src_host=src_host):
+            log.event = models.Log.EVENT_QUERY_ACCEPT
+            method = methods.Method(query['method_name'])
+            if method:
+                log.method_name = method.name
+                form = forms.form_factory(method=method, data=query)
+                if form.is_valid():
+                    log.router = form.cleaned_data['router']
+                    log.target = form.cleaned_data['target']
+                    response['raw'] = execute(form=form, method=method)
+                else:
+                    log.event = models.Log.EVENT_QUERY_INVALID
+                    log.error = 'form validation failure'
             else:
-                response = { 'output': 'Error' }
+                log.event = models.Log.EVENT_QUERY_INVALID
+                log.error = "invalid method name"
         else:
-            response = { 'output': 'Bad Command' }
+            log.event = models.Log.EVENT_QUERY_REJECT
+            log.error = 'invalid authorisation key'
+        response['err'] = log.error
+        log.save()
         return JsonResponse(response)
 
 
@@ -156,3 +153,28 @@ def get_src(request=None):
         else:
             address = unicode(request.META['REMOTE_ADDR'])
     return address
+
+
+def authorise(key=None, src_host=None):
+    if keys.AuthKey(src_host).validate(key):
+        count = models.Log.objects.filter(key=key).count()
+        if not settings.MAX_REQUESTS or count < settings.MAX_REQUESTS:
+            return True
+    return False
+
+
+def execute(form, method):
+    data = form.cleaned_data
+    router = data['router']
+    target = data['target']
+    if 'options' in data:
+        option = int(data['options'])
+    else:
+        option = None
+    lg = LookingGlass(router=router)
+    output = lg.execute(
+        method=method,
+        target=target,
+        option=option,
+    )
+    return output
