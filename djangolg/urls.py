fb6484fa4fecb6501e4f6e041fb4f9adbe4343ef
Ben Maddison <benm@workonline.co.za>
2016-08-07 10:05:15 +0200

    moved urls and ajax to use json view

--- djangolg/urls.py
+++ djangolg/urls.py
@@ -4,6 +4,5 @@ from djangolg import views
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name="djangolg-index"),
     url(r'^enter/$', views.AcceptTermsView.as_view(), name="djangolg-enter"),
-    url(r'^lg/$', views.LookingGlassHTMLView.as_view(), name="djangolg-lg"),
-    url(r'^json/$', views.LookingGlassJsonView.as_view(), name="djangolg-json"),
+    url(r'^lg/$', views.LookingGlassJsonView.as_view(), name="djangolg-lg"),
 ]
