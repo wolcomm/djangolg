fc6b313bb8e848126ca385b13992cf3362cdcbd7
Ben Maddison <benm@workonline.co.za>
2017-09-07 00:48:57 +0200

    fixed module docstrings

--- djangolg/methods/bgp_community.py
+++ djangolg/methods/bgp_community.py
@@ -11,7 +11,7 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations under
 # the License.
-"""Builtin method classes for djangolg."""
+"""BGP Community method class for djangolg."""
 
 from __future__ import print_function
 from __future__ import unicode_literals
